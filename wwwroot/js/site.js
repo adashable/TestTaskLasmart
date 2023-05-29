92e76856b2a3d79416f962f5bd52b36d4d40dd8b
Дарья <@DESKTOP-7NHEARB>
2023-05-29 23:50:22 +0600

    unusable func was deleted

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,9 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function Alya() {
-    alert('Yes');
-}
+
 function DrawPoints() {
     $.ajax({
         type: 'GET',
